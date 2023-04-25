19309947d621600186a2802a1bc3b494c3d7e9fb
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-04-25 21:21:25 +0530

    updated project

--- src/main/java/project/first/spring/controllers/BeerController.java
+++ src/main/java/project/first/spring/controllers/BeerController.java
@@ -2,7 +2,6 @@ package project.first.spring.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.first.spring.model.Beer;
--- src/main/java/project/first/spring/services/BeerServiceImpl.java
+++ src/main/java/project/first/spring/services/BeerServiceImpl.java
@@ -1,5 +1,7 @@
 package project.first.spring.services;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import project.first.spring.model.Beer;
