5dc9aba000e6717d41cbc8ab5d66be6fe8ba3a2b
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-04-26 21:08:04 +0530

    post method to save customer

--- src/main/java/project/first/spring/controllers/CustomerController.java
+++ src/main/java/project/first/spring/controllers/CustomerController.java
@@ -1,12 +1,15 @@
 package project.first.spring.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.first.spring.model.Beer;
 import project.first.spring.model.Customer;
 import project.first.spring.services.CustomerService;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +19,16 @@ import java.util.UUID;
 public class CustomerController {
 
     private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity addCustomer(@RequestBody Customer customer){
+        Customer savedCustomer = customerService.saveCustomer(customer);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location","api/v1/customer/"+savedCustomer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Customer> listCustomers(){
