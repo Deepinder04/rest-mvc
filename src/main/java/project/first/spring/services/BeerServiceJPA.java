09614433feeeb39d83bee0f1c833d86098eab3ae
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-05-04 21:09:05 +0530

    Added JPA implementations for customer,beer services

---
+++ src/main/java/project/first/spring/services/BeerServiceJPA.java
@@ -0,0 +1,49 @@
+package project.first.spring.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import project.first.spring.mappers.BeerMapper;
+import project.first.spring.model.BeerDTO;
+import project.first.spring.repositories.BeerRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Primary
+@AllArgsConstructor
+public class BeerServiceJPA implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+    @Override
+    public List<BeerDTO> listBeers() {
+        return null;
+    }
+
+    @Override
+    public BeerDTO getById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public BeerDTO saveBeer(BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void updatePatchById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+}
---
+++ src/main/java/project/first/spring/services/CustomerServiceJPA.java
@@ -0,0 +1,49 @@
+package project.first.spring.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import project.first.spring.mappers.CustomerMapper;
+import project.first.spring.model.CustomerDTO;
+import project.first.spring.repositories.CustomerRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Primary
+@AllArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    @Override
+    public List<CustomerDTO> customerList() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO getById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateById(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+}
