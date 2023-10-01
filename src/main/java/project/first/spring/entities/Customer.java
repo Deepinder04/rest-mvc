cbf8ed1a6ad648875a18904074639276af55b5ca
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-01 17:13:12 +0530

    Customer and Beer tests

--- src/main/java/project/first/spring/entities/Customer.java
+++ src/main/java/project/first/spring/entities/Customer.java
@@ -1,6 +1,8 @@
 package project.first.spring.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.ValueGenerationType;
@@ -20,7 +22,12 @@ public class Customer {
     @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
     @Column(length = 36,columnDefinition = "varchar",updatable = false,nullable = false)
     private UUID id;
+
+    @NotEmpty
+    @Size(max = 50)
+    @Column(length = 50)
     private String customerName;
+
     @Version
     private Integer version;
     private LocalDateTime createdAt;
