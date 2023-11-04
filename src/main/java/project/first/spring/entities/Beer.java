882ab41c83a398e9f4536d5b7674d32121f48995
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-11-04 10:42:43 +0530

    added openapi dependency

--- src/main/java/project/first/spring/entities/Beer.java
+++ src/main/java/project/first/spring/entities/Beer.java
@@ -1,7 +1,6 @@
 package project.first.spring.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
